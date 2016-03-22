b1939d66f1445fbd0e3b40e12b3d3d0b8b415b93
Niels Bokmans <niels@directshop.nl>
2016-03-22 12:08:53 +0100

    Fix naming

--- day2/foldleft/FoldLeftMain.scala
+++ day2/foldleft/FoldLeftMain.scala
@@ -6,26 +6,31 @@ package day2.foldleft
 object FoldLeftMain {
 
   def stringList = List[String]("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten")
+
   def intList = List[Int](1, 2, 3, 4)
+
   def list = List(1, 2, 3)
-  def main(args: Array[String]) : Unit = {
-    println("List[String] to string length with fold left: " + countStringArray(stringList))
-    println("Sum of List[Int]: " + countIntArraySum(intList))
-    println("Fold left sum of List[Int]: " + countIntArrayFoldLeft(intList))
-    println("Append List[String]: " + appendStringArray(stringList))
+
+  def main(args: Array[String]): Unit = {
+    println("List[String] to string length with fold left: " + countStringList(stringList))
+    println("Sum of List[Int]: " + countIntListSum(intList))
+    println("Fold left sum of List[Int]: " + countIntListFoldLeft(intList))
+    println("Append List[String]: " + appendStringList(stringList))
   }
 
-  def countIntArraySum(list : List[Int]) : Int = {
+  def countIntListSum(list: List[Int]): Int = {
     list.sum
   }
-  def countIntArrayFoldLeft(list : List[Int]) : Int = {
+
+  def countIntListFoldLeft(list: List[Int]): Int = {
     list.foldLeft(0)((sum, value) => sum + value)
   }
-  def countStringArray(list : List[String]) : Int = {
+
+  def countStringList(list: List[String]): Int = {
     list.foldLeft(0)((sum, str) => sum + str.length())
   }
 
-  def appendStringArray(list : List[String]) : String = {
+  def appendStringList(list: List[String]): String = {
     list.reduce(_ + ", " + _)
   }
 
